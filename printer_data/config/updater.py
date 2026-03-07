2c2cbcfc74bba6c0c19a2fcbb2a75b1794c60bec
pi <pi@voron-04b08f6>
2026-03-07 16:28:37 +0800

    New Backup on boot - 07/03/26 - 16:28:31

--- printer_data/config/updater.py
+++
@@ -1,6 +0,0 @@
-import os
-
-print('Updating Mainsail...')
-os.system('/usr/bin/python /home/pi/mainsail.py')
-print('Mainsail successfully updated!')
-print('Reload this page for the update to take effect.')
